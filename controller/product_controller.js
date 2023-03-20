a1b5a61b2d6d26172d4147f1cc62fda6c0bb5b52
lianamatshkalyan01 <liana.matshkalyan01@gmail.com>
2023-03-20 23:53:22 +0300

    finaly

--- controller/product_controller.js
+++ controller/product_controller.js
@@ -2,17 +2,46 @@ const sqlite3 = require('sqlite3').verbose()
 let db = new sqlite3.Database("database.db")
 const jwt = require('jsonwebtoken')
 
-function get_cart(req, res){
-    // const cart_id = req.body.cartid;
-    // const product_id = req.body.productid
-    
-    db.all("select * from cartitems", [], (err, data)=>{
+
+function get_cart(req,res){
+    const token= req.headers.authorization
+    const decoded=jwt.decode(token)
+    db.get("select * from users where username=?",[decoded.username],(err, data)=>{
         if(err){
             console.log(err)
         }
-        res.send(data)
+        db.all("select * from cartitems where cartid=?", [data.id], (err, data)=>{
+            if(err){
+                console.log(err)
+            }
+            res.send(data)
+        })
     })
 }
+
+// function get_cart(req, res){
+//     const token= req.headers.authorization
+//     const decoded=jwt.decode(token)
+//     db.all("select * from cartitems where cartid=?", [decoded.userid],(err, data)=>{
+//         if(err){
+//             console.log(err)
+//         }
+//         res.send(data)
+//     })
+// }
+
+// function post_cart(req,res){
+//     const  product_id= req.body.productid;
+//     const token= req.headers.authorization
+//     const decoded=jwt.decode(token)
+//     decoded.userid= req.body.cartid
+//     db.run("insert into cartitems(cartid, productid) values(?,?)",[decoded.userid, product_id], (err,data)=>{
+//         if(err){
+//             console.log(err)
+//         }
+//         res.send('Product added to cart');
+//     })
+// }
 
 function post_cart (req, res) {
     const  product_id= req.body.productid;
@@ -50,6 +79,19 @@ function post_cart (req, res) {
     });
 })
   };
+
+//   function delete_cart(req,res){
+//     const product_id = req.body.productid;
+//     const token= req.headers.authorization
+//     const decoded=jwt.decode (token)
+//     decoded.userid= req.body.cartid
+//     db.run("delete from cartitmes where cartid=? and productid=?", [decoded.userid, product_id], (err,data)=>{
+//         if(err){
+//             console.log(err)
+//         }
+//         res.send('Product deleted from cart');
+//     })
+//   }
 
 module.exports = {
  get_cart, post_cart, delete_cart
